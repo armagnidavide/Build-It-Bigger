e83b9d22071d57d96050cac5daf968d5a7e87cde
Arma <armagnidavide@gmail.com>
2018-01-13 16:04:28 +0100

    Create a Java library

---
+++ javalib/src/main/java/com/example/android/javalib/myClass.java
@@ -0,0 +1,7 @@
+package com.example.android.javalib;
+
+public class myClass {
+    public static String javaLibraryJoke(){
+        return "this Joke is super funny and comes from the Java Library";
+    }
+}
