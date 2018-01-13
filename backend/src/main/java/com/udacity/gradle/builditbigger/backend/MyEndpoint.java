5257d2f42a76b344c31b9bcb18468ca47f5d0952
Arma <armagnidavide@gmail.com>
2018-01-13 15:53:57 +0100

    Starting Point

---
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -0,0 +1,35 @@
+/*
+   For step-by-step instructions on connecting your Android application to this backend module,
+   see "App Engine Java Endpoints Module" template documentation at
+   https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/master/HelloEndpoints
+*/
+
+package com.udacity.gradle.builditbigger.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import static com.example.android.libjavajokes.myClass.javaLibraryJoke;
+
+/** An endpoint class we are exposing */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.builditbigger.gradle.udacity.com",
+                ownerName = "backend.builditbigger.gradle.udacity.com",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+    /** A simple endpoint method that takes a name and says Hi back */
+    @ApiMethod(name = "tellAJoke")
+    public MyBean tellAJoke() {
+        MyBean response = new MyBean();
+        response.setData("Hi, this joke is for you: "+"\""+javaLibraryJoke()+"\"");
+        return response;
+    }
+
+
+}
